67e4f8869529a4edf2f94e167a63a4d1db9dce4f
Anna <a.baranova@strategiedigitali.io>
2023-06-29 17:59:33 +0200

    Updated link

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const customLayer = {
 
   onAdd: function () {
     const ifcLoader = new IFCLoader();
-    ifcLoader.ifcManager.setWasmPath( '../../../' );
+    ifcLoader.ifcManager.setWasmPath( '"./"' );
     ifcLoader.load( './01.ifc', function ( model ) {
       scene.add( model );
     });
